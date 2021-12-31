dbd50834dd502a79bf680dbffb21a00a26af0a2f
dynmie <xbottam@gmail.com>
2021-12-30 17:07:24 -0800

    one hour long bug fix

--- src/main/java/co/rendernetwork/cosmoscore/events/listeners/MenuListener.java
+++ src/main/java/co/rendernetwork/cosmoscore/events/listeners/MenuListener.java
@@ -1,12 +1,13 @@
 package co.rendernetwork.cosmoscore.events.listeners;
 
+import co.rendernetwork.cosmoscore.gui.Button;
 import co.rendernetwork.cosmoscore.gui.IMenuAction;
 import co.rendernetwork.cosmoscore.gui.Menu;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.*;
 
 public class MenuListener implements Listener {
 
@@ -16,14 +17,29 @@ public class MenuListener implements Listener {
         if (!(event.getWhoClicked() instanceof Player)) return;
         if (event.getClickedInventory() == null) return;
         if (!(event.getClickedInventory().getHolder() instanceof Menu)) return;
-        if (event.getCurrentItem() == null || event.getCurrentItem().getType() == Material.AIR) return;
+
         event.setCancelled(true);
+        if (event.getCurrentItem() == null || event.getCurrentItem().getType() == Material.AIR) return;
 
         Menu menu = (Menu) event.getClickedInventory().getHolder();
+        Button button = menu.getButton(event.getRawSlot());
+        if (button == null) return;
 
-        for (IMenuAction action : menu.getButton(event.getRawSlot()).getActions())
+        for (IMenuAction action : button.getActions())
             action.execute((Player) event.getWhoClicked());
 
+    }
+
+    @EventHandler
+    public void onDrag(InventoryDragEvent event) {
+        if (event.getInventory().getHolder() instanceof Menu) event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onMove(InventoryMoveItemEvent event) {
+        if (event.getSource().getHolder() instanceof Menu) event.setCancelled(true);
+        if (event.getInitiator().getHolder() instanceof Menu) event.setCancelled(true);
+        if (event.getDestination().getHolder() instanceof Menu) event.setCancelled(true);
     }
 
 }
